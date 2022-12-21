8a2781ac3d33b3da0c62dd7774d31633f6c363a3
takahashi-shuta <121151844+takahashi-shuta@users.noreply.github.com>
2022-12-21 15:48:47 +0900

    戻り値変更？
    solveのを標準出力ではなく文字列を返すように変更。未確認。

--- kociemba/command_line.py
+++ kociemba/command_line.py
@@ -2,10 +2,19 @@ import sys
 from kociemba import solve
 
 
+# def main():
+#     if len(sys.argv) == 2:
+#         print(solve(sys.argv[1]))
+#     elif len(sys.argv) == 3:
+#         print(solve(sys.argv[1], sys.argv[2]))
+#     else:
+#         print('Usage: kociemba <cubestring> [resulting_cubestring]\nfor example:\nkociemba DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD BBURUDBFUFFFRRFUUFLULUFUDLRRDBBDBDBLUDDFLLRRBRLLLBRDDF')
+
 def main():
     if len(sys.argv) == 2:
-        print(solve(sys.argv[1]))
+        return solve(sys.argv[1])
     elif len(sys.argv) == 3:
-        print(solve(sys.argv[1], sys.argv[2]))
+        return solve(sys.argv[1], sys.argv[2])
     else:
         print('Usage: kociemba <cubestring> [resulting_cubestring]\nfor example:\nkociemba DRLUUBFBRBLURRLRUBLRDDFDLFUFUFFDBRDUBRUFLLFDDBFLUBLRBD BBURUDBFUFFFRRFUUFLULUFUDLRRDBBDBDBLUDDFLLRRBRLLLBRDDF')
+
